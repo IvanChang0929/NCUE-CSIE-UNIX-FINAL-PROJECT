bdd4bfea7cc62fa3408bf7d25787bdf7c7826c75
.. .. <jason0412.huang@gmail.com>
2026-05-24 14:46:37 +0800

    add status result

--- sandbox/include/result_writer.h
+++ sandbox/include/result_writer.h
@@ -6,6 +6,10 @@ typedef struct {
     int exit_code;          // 程式的回傳碼或 Signal 碼
     char stdout_buf[4096];  // 標準輸出緩衝區
     char stderr_buf[4096];  // 標準錯誤緩衝區
+
+    long time_ms;     // 執行時間 (毫秒)
+    long memory_kb; // 記憶體使用量 (KB)
+    char status_message[128];
 } StageResult;
 
 void write_combined_json(const char *job_id, StageResult *comp, StageResult *exec);
